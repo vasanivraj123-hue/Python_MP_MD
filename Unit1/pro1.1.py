6a1917abd394ba24864885e9d58d2a3db74846bd
vasanivraj123-hue <vasanivraj123@gmail.com>
2026-03-06 11:14:15 +0530

    Add files via upload

---
+++ Unit1/pro1.1.py
@@ -0,0 +1,9 @@
+#Write a program to input p, r, n and find out interest using simple input output statement.
+
+p=int(input('Eneter Amount :'))
+r=int(input('Eneter Rate :'))
+n=int(input('Eneter Year :'))
+
+si=p*r*n/100
+
+print('Simple Interest is :',si)
