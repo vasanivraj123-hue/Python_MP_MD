dbc1d5cd278820340b31bf75d6b29cc8c141cfa7
vasanivraj123-hue <vasanivraj123@gmail.com>
2026-03-13 11:08:56 +0530

    Add MyClass with two methods and usage example

--- Unit 3/1.py
+++ Unit 3/1.py
@@ -1,1 +1,12 @@
+class MyClass:
+   
+    def method_one(self):
+        print("This is Method One")
 
+    def method_two(self):
+        print("This is Method Two")
+
+obj = MyClass()
+
+obj.method_one()
+obj.method_two()
