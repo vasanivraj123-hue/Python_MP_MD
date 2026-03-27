5108272ce9752feeb3d8668c05d9954b4711797b
vasanivraj123-hue <vasanivraj123@gmail.com>
2026-03-27 11:30:10 +0530

    Update 1.py

--- Unit 4/1.py
+++ Unit 4/1.py
@@ -1,1 +1,11 @@
+import pandas as pd
+
+file_path = "student.xlsx"   
+df = pd.read_excel(file_path)
+
+print("Columns in the Excel file:")
+print(df.columns.tolist())
+
+print("\nData types of each column:")
+print(df.dtypes)
 
